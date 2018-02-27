8e39823689693ff07a48dbb9170f15d338755d92
Bet Mendoza <sbm2391@hotmail.com>
2018-02-27 11:22:37 -0600

    display map with your location

--- routes/item.js
+++ routes/item.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router  = express.Router();
+const TYPES   = require ('../models/item_types');
 
 /* Catalog index*/
 router.get('/', (req, res, next) => {
-  res.render('catalog/index', {user:req.user})
+  res.render('catalog/index', {user:req.user, types:TYPES})
 });
 
 module.exports = router;
